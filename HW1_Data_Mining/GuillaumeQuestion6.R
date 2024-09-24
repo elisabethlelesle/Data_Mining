ef825caa1e553957e9ec78f4de927d79c19fb4f9
gdrui01 <gdrui@hotmail.fr>
2024-09-24 19:00:06 +0800

    Question 6 finished

---
+++ HW1_Data_Mining/GuillaumeQuestion6.R
@@ -0,0 +1,27 @@
+###Question 6
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(MASS)
+library(readxl)
+library(forecast)
+library(arules)
+
+
+Asia_stock <- read_excel("Asia_Stock.xlsx")
+model <- lm(TAIWAN ~ ., data = Asia_stock)
+
+
+# Extract R-squared and Adjusted R-squared from the summary
+r_squared <- summary(model)$r.squared
+adj_r_squared <- summary(model)$adj.r.squared
+
+# View p-values for significance of each feature
+p_values <- summary(model)$coefficients[,4]
+
+print(r_squared)
+print(adj_r_squared)
+print(head(sort(p_values)))
+
+
